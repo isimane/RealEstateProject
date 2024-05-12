7f235ca4243923377e37c74c63486397ae19bacb
simane <simaneid05@gmail.com>
2024-05-12 18:55:44 +0100

    updated my js code

--- script.js
+++ script.js
@@ -48,13 +48,10 @@ unshowlist.addEventListener('click', () =>{
 form.addEventListener('submit', (e)=>{
     if( !input.value ){
         errorMessage.innerText = "Please fill out this field";
-        input.classList.add('outline-[#FF0000]')
-        input.classList.remove('outline-none')
-        // alert('Please enter a valid name');
+        input.classList.add('border-2','border-[#FF0000]')
         e.preventDefault() // to prevent the submission if the input is empty
     }else if(String(input.value) === ''){
-        errorMessage.innerText = "Please enter a valid address";
-        
+        errorMessage.innerText = "Please enter a valid address"; 
     }
      else{
         successMessage.innerText = "Great!!"
