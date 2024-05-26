328243a085ca808c3829852a9e369dc0ca72b82e
simane <simaneid05@gmail.com>
2024-05-26 20:27:46 +0100

    updated my code

--- script.js
+++ script.js
@@ -45,22 +45,24 @@ unshowlist.addEventListener('click', () =>{
     }
     // navbar.classList.toggle('hidden');
 });
-
-form.addEventListener('submit', (e)=>{
-    let regex = /[a-z]/ig ;
-    if(!input.value.trim() ){
-        errorMessage.innerText = "Please enter Your address!";
-        input.classList.add('border-2','border-[#FF0000]')
-        e.preventDefault() // to prevent the submission if the input is empty
-    }
-    else if(!regex.test(input.value)){
-        errorMessage.innerText = "Please enter a valid address!"
-        input.classList.add('border-2','border-[#FF0000]')
-        e.preventDefault();
-    }
-     else{
-        successMessage.innerText = ""
-    };
-
-
+form.addEventListener('click', ()=>{
+    input.classList.add('border-1','border-[#0000FF]');
+        form.addEventListener('submit', (e)=>{
+            let regex = /[a-z]/ig ;
+            
+            if(!input.value.trim() ){
+                input.value.clear();
+                errorMessage.innerText = "Please enter Your address!";
+                input.classList.add('border-2','border-[#FF0000]')
+                e.preventDefault() // to prevent the submission if the input is empty
+            }
+            else if(!regex.test(input.value)){
+                errorMessage.innerText = "Please enter a valid address!"
+                input.classList.add('border-2','border-[#FF0000]')
+                e.preventDefault();
+            }
+            else{
+                successMessage.innerText = ""
+            };
+        })
 })
